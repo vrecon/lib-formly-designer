4f17f9fe387972e3abe0d854999baef14fe2ac24
w4web <w4websolution@gmail.com>
2022-03-10 16:14:07 +0530

    Done.

--- projects/ngx-formly-designer-test/src/app/app.module.ts
+++ projects/ngx-formly-designer-test/src/app/app.module.ts
@@ -1,20 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxFormlyDesignerModule } from 'ngx-formly-designer';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyTabsModule } from 'ngx-formly-tabs';
+
+import { FormlyDesignerModule } from 'ngx-formly-designer';
+
+import { config, fieldComponents } from './config';
+import { designerConfig } from './designer-config';
+
 import { AppComponent } from './app.component';
+import { ExpanderComponent } from './components/expander.component';
+import { FieldsService, FormlyDesignerService } from 'ngx-formly-designer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpanderComponent,
+
+    fieldComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgxFormlyDesignerModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    FormlyTabsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot(config),
+
+    FormlyDesignerModule.forRoot(designerConfig)
   ],
-  providers: [],
+  providers: [FormlyDesignerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- projects/ngx-formly-designer/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-formly-designer
- */
-
-export * from './lib/ngx-formly-designer.service';
-export * from './lib/ngx-formly-designer.component';
-export * from './lib/ngx-formly-designer.module';
